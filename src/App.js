17e6396f982353683b83b872b237434394a1a444
Reebby <rebbyoshiobughie@gmail.com>
2020-09-10 11:37:52 +0100

    added majority of the files and changes

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.js';
+import { Header } from './components/header/header.component'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        {/* <Route exact path='/shop/hats' component={HatsPage} /> */}
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
